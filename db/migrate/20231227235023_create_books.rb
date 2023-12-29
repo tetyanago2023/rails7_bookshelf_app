bfe3098587834aba643efa71383f148d4aba0828
go2023 <tetyanago2018@gmail.com>
2023-12-28 17:58:09 -0800

    Book stuff were added

---
+++ db/migrate/20231227235023_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.text :description
+      t.integer :reading_status
+
+      t.timestamps
+    end
+  end
+end
